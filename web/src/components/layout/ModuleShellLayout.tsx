fd77ec888d9c92a4bd30e85ae22c5bda1ea82438
Achraf <achrafkt4@gmail.com>
2026-08-13 15:49:20 +0100

    style

--- web/src/components/layout/ModuleShellLayout.tsx
+++ web/src/components/layout/ModuleShellLayout.tsx
@@ -27,7 +27,9 @@ export function ModuleShellLayout({ children }: { children: React.ReactNode }) {
       <AppHeader />
       <div className="flex flex-1 overflow-hidden">
         <NavRail />
-        <div className="flex-1 overflow-y-auto">{!mounted || !currentUser ? null : children}</div>
+        <div className="flex-1 overflow-y-auto rounded-t-[20px] bg-white">
+          {!mounted || !currentUser ? null : children}
+        </div>
       </div>
     </div>
   );
